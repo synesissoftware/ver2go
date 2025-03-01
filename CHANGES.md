e39e2c2fe65ad1bff5ca3ae7a189f84826e8f27d
Matt Wilson <152443343+mwsis@users.noreply.github.com>
2025-03-01 14:51:12 +1100

    Fix (#3)
    * ~ fix
    
    * ~ project files update

--- CHANGES.md
+++ CHANGES.md
@@ -1,7 +1,12 @@
 # **ver2go** Changes <!-- omit in toc -->
 
 
-## 0.0.1 - 26th February 2025
+## 0.0.0-alpha3 - 1st March 2025
+
+* fix;
+
+
+## 0.0.0-alpha2 - 26th February 2025
 
 * tidying;
 
--- version.go
+++ version.go
@@ -7,7 +7,7 @@
 
 /*
  * Created: 13th February 2025
- * Updated: 26th February 2025
+ * Updated: 1st March 2025
  */
 
 package ver2go
@@ -16,7 +16,7 @@ const (
 	VersionMajor uint16 = 0
 	VersionMinor uint16 = 0
 	VersionPatch uint16 = 0
-	VersionAB    uint16 = 0x4002
+	VersionAB    uint16 = 0x4003
 	Version      uint64 = (uint64(VersionMajor) << 48) + (uint64(VersionMinor) << 32) + (uint64(VersionPatch) << 16) + (uint64(VersionAB) << 0)
 )
 
